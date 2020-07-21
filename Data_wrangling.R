b88a52028d3980341019dd48ad07cfb01a4a8256
gabbie-sfbbo <gburns@sfbbo.org>
2020-07-21 19:23:42 +0000

    Figured out how to connect to online db

--- Data_wrangling.R
+++ Data_wrangling.R
@@ -3,6 +3,23 @@
 
 ##load libraries
 library(stringr) ##required for string manipulation
+library(DBI)
+library(RPostgreSQL)
+
+# Extract CCFS data from the banding database ----
+# Read in DB connection details
+db_details <- read.csv("database_config.csv")
+
+# Connect to a specific postgres database
+con <- dbConnect(dbDriver("PostgreSQL"),dbname = db_details$Database, 
+                 host = db_details$Host,
+                 port = db_details$Port,
+                 user = db_details$User,
+                 password = db_details$Password)
+
+birds_test <- dbGetQuery(con, statement = paste("SELECT * FROM banding.banding_records LIMIT 10"));
+dbDisconnect(con)
+
 
 ##load weather data
 skip<-which(str_detect(string = read.csv("data/daily_weather_data_1983-2019.csv")[,1], pattern = "END.HEADER"))+1 ##skip all info in header section
