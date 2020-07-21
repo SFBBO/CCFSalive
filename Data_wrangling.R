6ec2d62bc517f17babc5e32abc6ec2e6bb7fa8fc
mtarjan <max.tarjan@gmail.com>
2020-07-21 12:13:26 -0700

    comment wrangling code

--- Data_wrangling.R
+++ Data_wrangling.R
@@ -3,5 +3,7 @@
 
 ##load libraries
 library(stringr) ##required for string manipulation
+
+##load weather data
 skip<-which(str_detect(string = read.csv("data/daily_weather_data_1983-2019.csv")[,1], pattern = "END.HEADER"))+1 ##skip all info in header section
 weather<-read.csv("data/daily_weather_data_1983-2019.csv", skip = skip)
