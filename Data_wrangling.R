99ff9160d28fef0f901691cbcdf34240dd0cb2eb
mtarjan <max.tarjan@gmail.com>
2020-07-21 12:11:14 -0700

    load weather data

---
+++ Data_wrangling.R
@@ -0,0 +1,7 @@
+##CCFSlive
+##Data wrangling
+
+##load libraries
+library(stringr) ##required for string manipulation
+skip<-which(str_detect(string = read.csv("data/daily_weather_data_1983-2019.csv")[,1], pattern = "END.HEADER"))+1 ##skip all info in header section
+weather<-read.csv("data/daily_weather_data_1983-2019.csv", skip = skip)
